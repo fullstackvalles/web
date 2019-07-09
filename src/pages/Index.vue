7d947ce636139dab7f448026ffd73814550c9de8
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2019-07-09 23:24:22 +0200

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout>
+    <p class="home-links">
+      <a href="https://slack.fullstackvalles.dev" target="_blank" rel="noopener">Enter Slack</a>
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
